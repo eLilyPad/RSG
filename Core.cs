08256fe2e871acaab9c0002296f06b6da3fd411e
eLilyPad <elizabethbryce00@gmail.com>
2025-11-29 15:57:51 +0000

    - added keybinds and display them on the input settings tab

--- Core.cs
+++ Core.cs
@@ -19,6 +19,10 @@ public sealed partial class Core : Node
 		Name = nameof(Core);
 		this.Add(Nonogram, Menu);
 
+		Input.Bind((Key.Escape, Menu.Step, "Toggle Main Menu"));
+
+		Menu.Settings.Input.InputsContainer.RefreshBindings();
+
 		foreach (var puzzle in PuzzleManager.GetSavedPuzzles())
 		{
 			//GD.Print($"Found saved puzzle: {puzzle.Expected.Name} of size {puzzle.Expected.Size}");
@@ -26,12 +30,7 @@ public sealed partial class Core : Node
 	}
 	public override void _Input(InputEvent input)
 	{
-		switch (input)
-		{
-			case InputEventKey { Keycode: Key.Escape, Echo: false, Pressed: true }:
-				Menu.Step();
-				break;
-		}
+		Input.RunEvent(input);
 	}
 }
 
--- Input.cs
+++ Input.cs
@@ -4,6 +4,11 @@ namespace RSG.UI;
 
 public sealed partial class Input : Resource
 {
+	private class KeyBinds
+	{
+		public readonly Dictionary<Key, Action> Actions = [];
+		public readonly Dictionary<Key, string> Names = [];
+	}
 	public sealed partial class Container : ScrollContainer
 	{
 		public KeyBindsContainer InputsContainer { get; } = new KeyBindsContainer { Name = "KeyBindsContainer" };
@@ -24,6 +29,58 @@ public sealed partial class Input : Resource
 				this.Add(ResetButton, BindingsContainer)
 					.SizeFlags(horizontal: SizeFlags.Fill, vertical: SizeFlags.Fill);
 			}
+
+			public void RefreshBindings()
+			{
+				foreach (Node child in BindingsContainer.GetChildren())
+				{
+					BindingsContainer.RemoveChild(child);
+					child.QueueFree();
+				}
+				foreach ((Key key, string name) in GetBindings())
+				{
+					RichTextLabel keyLabel = new RichTextLabel() { Text = $"{name} : {key}", FitContent = true }
+					.SizeFlags(horizontal: SizeFlags.ExpandFill, vertical: SizeFlags.Fill);
+					BindingsContainer.AddChild(keyLabel);
+				}
+			}
 		}
 	}
+
+	public static IEnumerable<(Key key, string name)> GetBindings()
+	{
+		foreach (Key key in Bindings.Actions.Keys)
+		{
+			string name = Bindings.Names[key];
+			yield return (key, name);
+		}
+	}
+
+	public static void Unbind(params Span<Key> bindings)
+	{
+		foreach (Key key in bindings)
+		{
+			Bindings.Actions.Remove(key);
+			Bindings.Names.Remove(key);
+		}
+	}
+	public static void Bind(params Span<(Key key, Action action, string name)> bindings)
+	{
+		foreach (var (key, action, name) in bindings)
+		{
+			Bindings.Actions[key] = action;
+			Bindings.Names[key] = name;
+		}
+	}
+	public static void RunEvent(InputEvent input)
+	{
+		if (input is not InputEventKey keyEvent || !keyEvent.Pressed) return;
+		Key key = keyEvent.Keycode;
+		if (Bindings.Actions.TryGetValue(key, out Action? binding))
+		{
+			binding();
+		}
+	}
+
+	private static KeyBinds Bindings => field ??= new();
 }
