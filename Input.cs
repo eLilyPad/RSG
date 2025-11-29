b1afdb222f584d67f20a15f23ead31dac579f0d5
eLilyPad <elizabethbryce00@gmail.com>
2025-11-29 03:02:16 +0000

    fixed layout and moved keybind to a container class

--- Input.cs
+++ Input.cs
@@ -6,14 +6,25 @@ public sealed partial class Input : Resource
 {
 	public sealed partial class Container : ScrollContainer
 	{
-		public Button ResetButton { get; } = new() { Text = "Reset" };
-		public VBoxContainer InputsContainer { get; } = new();
-		public HBoxContainer Margin { get; } = new HBoxContainer()
-		.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize, 10);
+		public KeyBindsContainer InputsContainer { get; } = new KeyBindsContainer { Name = "KeyBindsContainer" };
+		public VBoxContainer MainContainer { get; } = new VBoxContainer() { Name = "Container" }
+			.SizeFlags(horizontal: SizeFlags.ExpandFill, vertical: SizeFlags.ExpandFill);
 		public override void _Ready()
 		{
-			Name = "InputContainer";
-			this.Add(Margin.Add(ResetButton, InputsContainer));
+			this.Add(MainContainer.Add(InputsContainer));
+		}
+
+		public sealed partial class KeyBindsContainer : VBoxContainer
+		{
+			public Button ResetButton { get; } = new() { Name = "Reset Button", Text = "Reset" };
+			public HBoxContainer BindingsContainer { get; } = new HBoxContainer { Name = "Bindings Container" }
+			.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize);
+
+			public override void _Ready()
+			{
+				this.Add(ResetButton, BindingsContainer)
+					.SizeFlags(horizontal: SizeFlags.Fill, vertical: SizeFlags.Fill);
+			}
 		}
 	}
 }
