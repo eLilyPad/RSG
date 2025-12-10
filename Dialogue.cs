c6a1bb3a805694a9ce02939d660314a4563276af
eLilyPad <elizabethbryce00@gmail.com>
2025-12-10 05:16:13 +0000

    - used premade values for profiles and backgrounds

--- Dialogue.cs
+++ Dialogue.cs
@@ -8,19 +8,18 @@ public static class DialogueBuilder
 {
 	public static Dialogues BuildDialogues(this Dialogues dialogues)
 	{
-		DialogueResources resources = Dialogues.Resources;
 		dialogues.SingleSpeaker(
 			Name: Intro,
 			Title: "Little Green Dude",
 			(
 				"hello, did you know... i'm quite green",
-				new Profile(resources.Profile),
-				new Background(resources.Background1)
+				Profile.LGD,
+				Background.Background1
 			),
 			"if only I was blue, but this is the hand i've been dealt",
 			(
 				"what?",
-				new Background(resources.Background2)
+				Background.Background2
 			),
 			"OH, a little cat has graced you with there presence",
 			"Any way, i'm going to go now...",
@@ -35,8 +34,15 @@ public static class DialogueBuilder
 public abstract record Dialogue
 {
 	public const string Intro = "intro";
-	public readonly record struct Background(CompressedTexture2D Value);
-	public readonly record struct Profile(CompressedTexture2D Value);
+	public readonly record struct Background(CompressedTexture2D Value)
+	{
+		public static Background Background1 { get; } = new(Dialogues.Resources.Background1);
+		public static Background Background2 { get; } = new(Dialogues.Resources.Background2);
+	}
+	public readonly record struct Profile(CompressedTexture2D Value)
+	{
+		public static Profile LGD { get; } = new(Dialogues.Resources.Profile);
+	}
 	public readonly record struct Message(string Title, string Text);
 	public readonly record struct Speech(ReadOnlyMemory<Message> Messages);
 }
