80c89a60d2181210530ec177346720a86987db1c
eLilyPad <elizabethbryce00@gmail.com>
2025-12-10 04:09:43 +0000

    - dialoging

--- Dialogue.cs
+++ Dialogue.cs
@@ -3,17 +3,12 @@ using Godot;
 namespace RSG;
 
 using static Dialogue;
-using SpeechTemplate = OneOf<
-	string,
-	(string text, Dialogue.Background background),
-	(string text, Dialogue.Profile profile),
-	(string text, Dialogue.Profile profile, Dialogue.Background background)
->;
 
 public static class DialogueBuilder
 {
-	public static Dialogues BuildDialogues(this Dialogues dialogues, DialogueResources resources)
+	public static Dialogues BuildDialogues(this Dialogues dialogues)
 	{
+		DialogueResources resources = Dialogues.Resources;
 		dialogues.SingleSpeaker(
 			Name: Intro,
 			Title: "Little Green Dude",
@@ -34,131 +29,6 @@ public static class DialogueBuilder
 			"Well that's unfortunate for you I'm off..."
 		);
 		return dialogues;
-	}
-}
-
-public sealed class Dialogues
-{
-	private class CurrentDialogue
-	{
-		public required DialogueContainer Container { get; init; }
-		public Speech Speech
-		{
-			get; set
-			{
-				field = value;
-				SpeechIndex = 0;
-			}
-		}
-		public int SpeechIndex { get; private set; }
-		public string Name { get; internal set; } = Intro;
-
-		public void Start()
-		{
-			Container.Show();
-			SpeechIndex = 0;
-			ReadOnlySpan<Message> messages = Speech.Messages.Span;
-			Display(messages[SpeechIndex]);
-		}
-		public void Next()
-		{
-			if (!Container.Visible) { return; }
-			SpeechIndex++;
-			ReadOnlySpan<Message> messages = Speech.Messages.Span;
-			if (messages.Length <= SpeechIndex)
-			{
-				Container.Hide();
-				return;
-			}
-			Display(messages[SpeechIndex]);
-		}
-		private void Display(Message message)
-		{
-			Container.Title.Text = message.Title;
-			Container.Message.Text = message.Text;
-			if (!Instance.SpeechExtras.TryGetValue(Name, out SpeechExtra? extras)) return;
-			if (extras.Backgrounds.TryGetValue(message.Text, out CompressedTexture2D? background))
-			{
-				Container.Background.Texture = background;
-			}
-			if (extras.Profiles.TryGetValue(message.Text, out CompressedTexture2D? profile))
-			{
-				Container.Profile.Texture = profile;
-			}
-
-		}
-	}
-	internal record SpeechExtra
-	{
-		public Dictionary<string, CompressedTexture2D> Backgrounds { get; } = [];
-		public Dictionary<string, CompressedTexture2D> Profiles { get; } = [];
-	}
-	public static DialogueContainer Container => field ??= new DialogueContainer
-	{
-		Visible = true,
-		Resources = DialogueResources
-	}
-		.Preset(preset: Control.LayoutPreset.FullRect, resizeMode: Control.LayoutPresetMode.KeepSize);
-	public static DialogueResources DialogueResources => field ??= Core.DialoguesPath.LoadOrCreateResource<DialogueResources>();
-
-	public static Dialogues Instance => field ??= new();
-	private static CurrentDialogue Current => field ??= new() { Container = Container };
-
-	public static void Start(string name)
-	{
-		if (!Instance.Speeches.TryGetValue(name, out Speech speech)) return;
-		Current.Speech = speech;
-		Current.Name = name;
-		Current.Start();
-	}
-	public static void Next() => Current.Next();
-
-	private Dictionary<string, Speech> Speeches { get; } = [];
-	private Dictionary<string, SpeechExtra> SpeechExtras { get; } = [];
-	private Dialogues()
-	{
-	}
-	public Speech SingleSpeaker(
-		string Name,
-		string Title,
-		params ReadOnlySpan<SpeechTemplate> textMessages
-	)
-	{
-		Assert(Instance is not null, "Instance is null");
-		Assert(Name is not null, "Name is null");
-
-		int index = 0;
-		Message[] messages = new Message[textMessages.Length];
-		SpeechExtra extras = new();
-
-		foreach (SpeechTemplate message in textMessages)
-		{
-			message.Switch(AddText, AddBackground, AddProfile, AddAll);
-			index++;
-		}
-		SpeechExtras[Name] = extras;
-		return Speeches[Name] = new Speech(Messages: messages);
-
-		void AddText(string text) => messages[index] = new Message(Title, text);
-		void AddAll((string Text, Profile Profile, Background Background) value)
-		{
-			AddText(value.Text);
-			extras.Backgrounds[value.Text] = value.Background.Value;
-			extras.Profiles[value.Text] = value.Profile.Value;
-			GD.Print("adding profile and background");
-		}
-		void AddProfile((string Text, Profile Profile) value)
-		{
-			AddText(value.Text);
-			extras.Profiles[value.Text] = value.Profile.Value;
-			GD.Print("adding profile");
-		}
-		void AddBackground((string Text, Background Background) value)
-		{
-			AddText(value.Text);
-			extras.Backgrounds[value.Text] = value.Background.Value;
-			GD.Print("adding background");
-		}
 	}
 }
 
---
+++ Dialogues.cs
@@ -0,0 +1,116 @@
+using Godot;
+
+namespace RSG;
+
+using static Dialogue;
+
+using SpeechTemplate = OneOf<
+	string,
+	(string text, Dialogue.Background background),
+	(string text, Dialogue.Profile profile),
+	(string text, Dialogue.Profile profile, Dialogue.Background background)
+>;
+
+public sealed class Dialogues
+{
+	private class CurrentDialogue
+	{
+		public Speech Speech { get; internal set => (field, SpeechIndex) = (value, 0); }
+		public int SpeechIndex { get; set; }
+		public string Name { get; set; } = Intro;
+	}
+	private record Extras
+	{
+		public Dictionary<string, CompressedTexture2D> Backgrounds { get; } = [];
+		public Dictionary<string, CompressedTexture2D> Profiles { get; } = [];
+	}
+	public static DialogueContainer Container => field ??= new DialogueContainer { Visible = true, Resources = Resources }
+		.Preset(preset: Control.LayoutPreset.FullRect, resizeMode: Control.LayoutPresetMode.KeepSize);
+	public static DialogueResources Resources => field ??= Core.DialoguesPath.LoadOrCreateResource<DialogueResources>();
+	public static Dialogues Instance => field ??= new();
+
+	private static CurrentDialogue Current => field ??= new();
+
+	public static void Start(string name)
+	{
+		if (!Instance.Speeches.TryGetValue(name, out Speech speech)) return;
+		(Current.Speech, Current.SpeechIndex, Current.Name) = (speech, 0, name);
+		DisplayCurrent();
+	}
+	public static void Next()
+	{
+		if (!Container.Visible) { return; }
+		Current.SpeechIndex++;
+		DisplayCurrent();
+	}
+	private static void DisplayCurrent()
+	{
+		Container.Show();
+		ReadOnlySpan<Message> messages = Current.Speech.Messages.Span;
+		if (messages.Length <= Current.SpeechIndex)
+		{
+			Container.Hide();
+			return;
+		}
+		Instance.Display(messages[Current.SpeechIndex]);
+	}
+
+	private Dictionary<string, Speech> Speeches { get; } = [];
+	private Dictionary<string, Extras> SpeechExtras { get; } = [];
+	private Dialogues() { }
+	public Speech SingleSpeaker(
+		string Name,
+		string Title,
+		params ReadOnlySpan<SpeechTemplate> textMessages
+	)
+	{
+		Assert(Instance is not null, "Instance is null");
+		Assert(Name is not null, "Name is null");
+
+		int index = 0;
+		Message[] messages = new Message[textMessages.Length];
+		Extras extras = new();
+
+		foreach (SpeechTemplate message in textMessages)
+		{
+			message.Switch(AddText, AddBackground, AddProfile, AddAll);
+			index++;
+		}
+		SpeechExtras[Name] = extras;
+		return Speeches[Name] = new Speech(Messages: messages);
+
+		void AddText(string text) => messages[index] = new Message(Title, text);
+		void AddAll((string Text, Profile Profile, Background Background) value)
+		{
+			AddText(value.Text);
+			extras.Backgrounds[value.Text] = value.Background.Value;
+			extras.Profiles[value.Text] = value.Profile.Value;
+		}
+		void AddProfile((string Text, Profile Profile) value)
+		{
+			AddText(value.Text);
+			extras.Profiles[value.Text] = value.Profile.Value;
+		}
+		void AddBackground((string Text, Background Background) value)
+		{
+			AddText(value.Text);
+			extras.Backgrounds[value.Text] = value.Background.Value;
+		}
+	}
+
+	private void Display(Message message)
+	{
+		Container.Title.Text = message.Title;
+		string text = Container.Message.Text = message.Text;
+		if (!SpeechExtras.TryGetValue(Current.Name, out Extras? extras)) return;
+		if (extras.Backgrounds.TryGetValue(text, out CompressedTexture2D? background))
+		{
+			Container.Background.Texture = background;
+		}
+		if (extras.Profiles.TryGetValue(text, out CompressedTexture2D? profile))
+		{
+			Container.Profile.Texture = profile;
+		}
+
+	}
+}
