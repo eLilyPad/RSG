0e80a8ac300bf744dea801064b859001920caa9d
eLilyPad <elizabethbryce00@gmail.com>
2025-12-11 00:23:57 +0000

    - added another test speech

--- Dialogue.cs
+++ Dialogue.cs
@@ -17,15 +17,26 @@ public static class DialogueBuilder
 				Background.Background1
 			),
 			"if only I was blue, but this is the hand i've been dealt",
-			(
-				"what?",
-				Background.Background2
-			),
-			"OH, a little cat has graced you with there presence",
 			"Any way, i'm going to go now...",
 			"You didn't ask but im going to the 4th plane of existence, it's quite calming there",
 			"You haven't heard of it?",
 			"Well that's unfortunate for you I'm off..."
+		);
+
+		dialogues.SingleSpeaker(
+			Name: CatOnThePath,
+			Title: "Little Green Dude",
+			(
+				"hello, did you know... i'm quite green",
+				Profile.LGD,
+				Background.Background1
+			),
+			"if only I was blue, but this is the hand i've been dealt",
+			(
+				"what?",
+				Background.Background2
+			),
+			"OH, a little cat has graced you with there presence"
 		);
 		return dialogues;
 	}
@@ -33,7 +44,7 @@ public static class DialogueBuilder
 
 public abstract record Dialogue
 {
-	public const string Intro = "intro";
+	public const string Intro = "intro", CatOnThePath = "catOnThePath";
 	public readonly record struct Background(CompressedTexture2D Value)
 	{
 		public static Background Background1 { get; } = new(Dialogues.Resources.Background1);
