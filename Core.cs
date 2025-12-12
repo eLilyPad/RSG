34aac558325996fd6f37f249ed557466e7c02dde
eLilyPad <elizabethbryce00@gmail.com>
2025-12-12 06:37:48 +0000

    - moved UI nodes into there own container class

--- Core.cs
+++ Core.cs
@@ -6,54 +6,52 @@ namespace RSG;
 using UI;
 using Nonogram;
 
-public sealed partial class Core : Node
+public sealed partial class CoreContainer : AspectRatioContainer
 {
-	public const string
-	ColourPackPath = "res://Data/DefaultColours.tres",
-	DialoguesPath = "res://Data/Dialogues.tres"
-	;
-	public AspectRatioContainer Container { get; } = new AspectRatioContainer
-	{
-		Name = "Container",
-		Ratio = 16f / 9f,
-		StretchMode = AspectRatioContainer.StretchModeEnum.Fit,
-	}
-		.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize);
 	public TitleScreenContainer LoadingScreen { get; } = new TitleScreenContainer { Name = "Loading Screen", }
 		.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize);
 	public NonogramContainer Nonogram { get; } = new NonogramContainer { Name = "Nonogram" }
 		.SizeFlags(horizontal: SizeFlags.ExpandFill, vertical: SizeFlags.ExpandFill)
 		.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize);
-	public PuzzleSelector Levels { get; } = new() { Name = "Level Selector", Visible = false };
-	public DialogueContainer DialogueContainer { get; } = Dialogues.Container;
-
-	public MainMenu Menu => field ??= new() { Colours = Colours };
-	public ColourPack Colours => field ??= ColourPackPath.LoadOrCreateResource<ColourPack>();
-
+	public PuzzleSelector Levels { get; } = new PuzzleSelector { Name = "Level Selector", Visible = false };
+	public MainMenu Menu { get; } = new MainMenu { Name = "MainMenu", Colours = Core.Colours };
+	public override void _Ready() => this.Add(Nonogram, Menu, Dialogues.Container, LoadingScreen);
+}
+public sealed partial class Core : Node
+{
+	public const string
+	ColourPackPath = "res://Data/DefaultColours.tres",
+	DialoguesPath = "res://Data/Dialogues.tres";
+	public static ColourPack Colours => field ??= ColourPackPath.LoadOrCreateResource<ColourPack>();
+	public CoreContainer Container => field ??= new CoreContainer
+	{
+		Name = "Core UI",
+		Ratio = 16f / 9f,
+		StretchMode = AspectRatioContainer.StretchModeEnum.Fit,
+	}.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize, 40);
 
 	public override void _Ready()
 	{
 		Name = nameof(Core);
-		this.Add(Container.Add(Nonogram, Menu, DialogueContainer, LoadingScreen));
-
+		this.Add(Container);
 		Dialogues.Instance.BuildDialogues();
 
 		Input.Bind((Key.Escape, StepBack, "Toggle Main Menu"));
-		Menu.Settings.Input.InputsContainer.RefreshBindings();
+		Container.Menu.Settings.Input.InputsContainer.RefreshBindings();
 
-		Menu.Init(Colours);
-		Nonogram.Init(Menu);
+		Container.Menu.Init(Colours);
+		Container.Nonogram.Init(Container.Menu);
 
 		DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
 
-		void StepBack() => Menu.StepBack(Menu.Settings, Menu.Levels, Menu.Dialogues);
+		void StepBack() => Container.Menu.StepBack(Container.Menu.Settings, Container.Menu.Levels, Container.Menu.Dialogues);
 	}
 	public override void _Input(InputEvent input)
 	{
 		if (!input.IsPressed()) return;
-		if (LoadingScreen.Visible)
+		if (Container.LoadingScreen.Visible)
 		{
-			LoadingScreen.Hide();
+			Container.LoadingScreen.Hide();
 			return;
 		}
 		if (input is InputEventMouseButton { Pressed: true })
@@ -62,27 +60,6 @@ public sealed partial class Core : Node
 			return;
 		}
 		Input.RunEvent(input);
-	}
-	public sealed partial class TitleScreenContainer : PanelContainer
-	{
-		public ColorRect Background { get; } = new ColorRect
-		{
-			Name = "Background",
-			Color = Colors.Aquamarine,
-		}.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize);
-		public RichTextLabel LoadingText { get; } = new RichTextLabel
-		{
-			Name = "Loading Text",
-			BbcodeEnabled = true,
-			Text = "[color=black][font_size=60] Press Anything To Continue...",
-			HorizontalAlignment = HorizontalAlignment.Center,
-			VerticalAlignment = VerticalAlignment.Center,
-			FitContent = true,
-		}.Preset(preset: LayoutPreset.Center, resizeMode: LayoutPresetMode.KeepSize);
-		public override void _Ready()
-		{
-			this.Add(Background, LoadingText);
-		}
 	}
 }
 
