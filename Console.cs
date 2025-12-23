d2c40c93d2e719846ee5f7f5544b548c4c693f8f
eLilyPad <elizabethbryce00@gmail.com>
2025-12-23 23:11:44 +0000

    - removed CommandKey, replaced to underlying string value

--- Console.cs
+++ Console.cs
@@ -3,12 +3,12 @@ namespace RSG;
 public static class ConsoleExtensions
 {
 	public static void Run(
-		this IDictionary<Console.CommandKey, Dictionary<Console.CommandKey, Console.Command>> modules,
+		this IDictionary<string, Dictionary<string, Console.Command>> modules,
 		Console.CommandInput input
 	)
 	{
 		Console.Command? command = null;
-		_ = modules.TryGetValue(input.Prefix, value: out Dictionary<Console.CommandKey, Console.Command>? module);
+		_ = modules.TryGetValue(input.Prefix, value: out Dictionary<string, Console.Command>? module);
 		_ = module?.TryGetValue(input.Phrase, value: out command);
 
 		if (command is null) { return; }
@@ -17,14 +17,14 @@ public static class ConsoleExtensions
 			command.Default();
 			return;
 		}
-		IEnumerable<Console.CommandKey> flags = input.Flags.Where(command.Flags.ContainsKey);
-		IEnumerable<KeyValuePair<Console.CommandKey, object>> props = input.Properties
+		IEnumerable<string> flags = input.Flags.Where(command.Flags.ContainsKey);
+		IEnumerable<KeyValuePair<string, object>> props = input.Properties
 			.Where(k => command.Properties.ContainsKey(k.Key));
-		foreach (Console.CommandKey key in flags)
+		foreach (string key in flags)
 		{
 			command.Flags[key]();
 		}
-		foreach ((Console.CommandKey key, object obj) in props)
+		foreach ((string key, object obj) in props)
 		{
 			command.Properties[key](obj);
 		}
@@ -45,27 +45,14 @@ public static class ConsoleExtensions
 
 public sealed record Console
 {
-	public sealed record Command(CommandKey Key)
+	public sealed record Command
 	{
-		public static Dictionary<CommandKey, Command> ForGame(Action quit)
-		{
-			return new Dictionary<CommandKey, Command>
-			{
-				["g"] = new(Key: "quit") { Default = quit },
-				["s"] = new(Key: "audio") { },
-			};
-		}
 		public Action Default { get; init; } = static () => { };
-		public Dictionary<CommandKey, Action<object>> Properties { get; init; } = [];
-		public Dictionary<CommandKey, Action> Flags { get; init; } = [];
-	}
-	public readonly record struct CommandKey(string ID = "")
-	{
-		public static implicit operator CommandKey(string Key) => new(Key);
+		public Dictionary<string, Action<object>> Properties { get; init; } = [];
+		public Dictionary<string, Action> Flags { get; init; } = [];
 	}
 	public readonly record struct CommandInput
 	{
-		public static implicit operator CommandInput(string input) => Parse(input);
 		public const string FlagPrefix = "--";
 		public const string PropertiesPrefix = @"p-";
 		public static CommandInput Parse(string input)
@@ -83,55 +70,64 @@ public sealed record Console
 				_ => new CommandInput()
 			};
 
-			static IReadOnlyList<CommandKey> flags(string s) => [
+			static IReadOnlyList<string> flags(string s) => [
 				.. s.Split(separator: ' ')
-			.Where(predicate: a => a.StartsWith(FlagPrefix))
+				.Where(predicate: a => a.StartsWith(FlagPrefix))
 			];
 			static IReadOnlyList<object> args(string s) => [
 				.. s.Split(separator: ' ')
-			.Where(static s => s is not FlagPrefix)
-			.Select(static s => Convert.ChangeType(value: s, conversionType: s.ParseInput().GetType()))
+				.Where(static s => s is not FlagPrefix)
+				.Select(static s => Convert.ChangeType(value: s, conversionType: s.ParseInput().GetType()))
 			];
-			static Dictionary<CommandKey, object> props(string s) => s.Split(' ')
+			static Dictionary<string, object> props(string s) => s.Split(' ')
 				.Where(static a => a.StartsWith(PropertiesPrefix) && a.Contains('='))
 				.Select(static a => a.Split('=', 2))
 				.Where(static a => a.Length == 2)
-				.Select(static a => new KeyValuePair<CommandKey, object>(key: a[0], value: a[1].ParseInput()))
+				.Select(static a => new KeyValuePair<string, object>(key: a[0], value: a[1].ParseInput()))
 				.ToDictionary();
 		}
-		private CommandInput(CommandKey prefix = new(), CommandKey phrase = new())
+		private CommandInput(string prefix = "", string phrase = "")
 		{
 			Prefix = prefix;
 			Phrase = phrase;
 		}
-		public readonly CommandKey Prefix, Phrase;
-		public IReadOnlyList<CommandKey> Flags { get; init; } = [];
+		public readonly string Prefix, Phrase;
+		public IReadOnlyList<string> Flags { get; init; } = [];
 		public IReadOnlyList<object> Args { get; init; } = [];
-		public IReadOnlyDictionary<CommandKey, object> Properties { get; init; } = new Dictionary<CommandKey, object>();
+		public IReadOnlyDictionary<string, object> Properties { get; init; } = new Dictionary<string, object>();
 
 		public readonly bool IsEmpty() => Flags.Count == 0 && Args.Count == 0 && Properties.Count == 0;
 	}
 
+	public static void Add(string prefix, params ReadOnlySpan<(string, Command)> configs)
+	{
+		Instance.Modules[prefix] = [];
+		foreach ((string input, Command command) in configs)
+		{
+			Instance.Modules[prefix][input] = command;
+		}
+	}
+
 	public static Console Instance { get; } = new();
 
-	public Dictionary<CommandKey, Dictionary<CommandKey, Command>> Modules { private get; init; } = [];
+	public IEnumerable<string> Prefixes => [.. Modules.Keys];
+	public Dictionary<string, Dictionary<string, Command>> Modules { private get; init; } = [];
+
 
 	public void Run(CommandInput input) => Modules.Run(input);
 	public void Submitted(string input) => Run(CommandInput.Parse(input));
 	public IEnumerable<string> Suggestions(string input)
 	{
 		CommandInput commandInput = CommandInput.Parse(input);
-		if (!Modules.TryGetValue(commandInput.Prefix, out Dictionary<CommandKey, Command>? module))
+		if (!Modules.TryGetValue(commandInput.Prefix, out Dictionary<string, Command>? module))
 		{
 			return Modules.Keys
-				.Select(k => k.ID)
-				.Where(k => k.StartsWith(commandInput.Prefix.ID, StringComparison.OrdinalIgnoreCase));
+				.Where(prefix => prefix.StartsWith(commandInput.Prefix, StringComparison.OrdinalIgnoreCase));
 		}
-		if (commandInput.Phrase.ID.Length == 0)
+		if (commandInput.Phrase is { Length: 0 })
 		{
 			return module.Keys
-				.Select(k => k.ID)
-				.Where(k => k.StartsWith(commandInput.Phrase.ID, StringComparison.OrdinalIgnoreCase));
+				.Where(key => key.StartsWith(commandInput.Phrase, StringComparison.OrdinalIgnoreCase));
 		}
 		return [];
 	}
