70a217f5dc164265b76f1de8ffff313d629b0620
eLilyPad <elizabethbryce00@gmail.com>
2025-12-25 03:02:31 +0000

    - added more colours

--- ColourPack.cs
+++ ColourPack.cs
@@ -9,6 +9,9 @@ public sealed partial class ColourPack : Resource, Nonogram.IColours
 	[Export] public Color MainMenuBackground { get; private set; } = Colors.Black;
 
 	[Export] public Color NonogramBackground { get; private set; } = Colors.DarkOliveGreen;
+	[Export] public Color NonogramHintBackground2 { get; private set; } = Colors.BlanchedAlmond;
+	[Export] public Color NonogramHintBackground1 { get; private set; } = Colors.FloralWhite;
 	[Export] public Color NonogramTileBackground2 { get; private set; } = Colors.BlanchedAlmond;
 	[Export] public Color NonogramTileBackground1 { get; private set; } = Colors.FloralWhite;
+	[Export] public Color NonogramTileBackgroundFilled { get; private set; } = Colors.Gold;
 }
