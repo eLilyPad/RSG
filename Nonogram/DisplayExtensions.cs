d65a22ebdfff0fb093d732f4db057e02634d6d73
eLilyPad <elizabethbryce00@gmail.com>
2026-01-02 19:44:10 +0000

    - added view update - removed tile exstentions

--- Nonogram/DisplayExtensions.cs
+++ Nonogram/DisplayExtensions.cs
@@ -80,6 +80,34 @@ public static class DisplayExtensions
 	{
 		return tiles.CalculateHints(position, selector: value => value.Button.Text is FillText ? 1 : 0);
 	}
+	internal static void UpdateView(this Display display, SaveData save, Tiles tiles, Hints hints)
+	{
+		IEnumerable<HintPosition> hintValues = HintPosition.AsRange(display.TilesGrid.Columns = save.Size);
+		IEnumerable<Vector2I> tileValues = (Vector2I.One * save.Size).GridRange();
+
+		bool firstTile = true;
+		foreach (Vector2I position in tileValues)
+		{
+			Tile tile = tiles.GetOrCreate(position);
+			TileMode state = save.States.GetValueOrDefault(key: position, defaultValue: TileMode.NULL);
+			tiles.ChangeMode(position, tile, input: state);
+			if (firstTile)
+			{
+				hints.TileSize = tile.Size;
+				firstTile = false;
+			}
+
+		}
+		foreach (HintPosition position in hintValues)
+		{
+			Hint hint = hints.GetOrCreate(position);
+			hints.ApplyText(position, hint);
+		}
+
+		tiles.Clear(exceptions: tileValues);
+		hints.Clear(exceptions: hintValues);
+		display.ResetTheme();
+	}
 	private static string CalculateHints<TValue>(
 		this IEnumerable<KeyValuePair<Vector2I, TValue>> tiles,
 		HintPosition position,
@@ -110,81 +138,4 @@ public static class DisplayExtensions
 		builder.Append(side.AsFormat());
 		run = 0;
 	}
-}
-public static class TileExtensions
-{
-	public static void StyleTileBackground(
-		this Button button,
-		Vector2I position,
-		IColours colours,
-		StyleBoxFlat? style = null,
-		TileMode? mode = null
-	)
-	{
-		const int chunkSize = 5;
-		const string themeName = "normal";
-		style ??= button.GetThemeStylebox(themeName).Duplicate() as StyleBoxFlat;
-		if (style is null) return;
-		int chunkIndex = position.X / chunkSize + position.Y / chunkSize;
-		Color filledTile = colours.NonogramTileBackgroundFilled;
-		Color background = chunkIndex % 2 == 0 ? colours.NonogramTileBackground1 : colours.NonogramTileBackground2;
-		Color blocked = background.Darkened(.4f);
-		style.BgColor = mode switch
-		{
-			TileMode.Filled => filledTile,
-			TileMode.Blocked => blocked,
-			_ => background
-		};
-		button.AddThemeStyleboxOverride(themeName, style);
-	}
-	public static bool Matches(this Button button, TileMode state)
-	{
-		return (button.Text is FillText && state is TileMode.Filled)
-			|| (button.Text is EmptyText && state is TileMode.Clear or TileMode.Blocked);
-	}
-	public static double ToDouble(this TileMode mode) => mode switch
-	{
-		TileMode.Blocked => 2,
-		TileMode.Filled => 1,
-		_ => 0,
-	};
-	public static TileMode Change(this TileMode input, TileMode currents) => input switch
-	{
-		TileMode.NULL => input,
-		TileMode mode when mode == currents => TileMode.Clear,
-		TileMode mode => mode
-	};
-	public static TileMode ToTileMode(this int mode) => mode switch
-	{
-		2 => TileMode.Blocked,
-		1 => TileMode.Filled,
-		_ => 0,
-	};
-	public static TileMode FromText(this string mode) => mode switch
-	{
-		BlockText => TileMode.Blocked,
-		FillText => TileMode.Filled,
-		EmptyText => TileMode.Clear,
-		_ => TileMode.NULL
-	};
-	public static void PlayAudio(this TileMode mode)
-	{
-		if (mode.AsAudioStream() is AudioStream stream) Audio.Buses.SoundEffects.Play(stream);
-	}
-	public static AudioStream? AsAudioStream(this TileMode mode) => mode switch
-	{
-		TileMode.Filled => Audio.NonogramSounds.FillTileClicked,
-		TileMode.Blocked => Audio.NonogramSounds.BlockTileClicked,
-		_ => null
-	};
-	public static string AsText<T>(this IImmutableDictionary<T, TileMode> modes, T position) where T : notnull
-	{
-		return modes.GetValueOrDefault(position, TileMode.Clear).AsText();
-	}
-	public static string AsText(this TileMode mode) => mode switch
-	{
-		TileMode.Blocked => BlockText,
-		TileMode.Filled => FillText,
-		_ => EmptyText,
-	};
 }
