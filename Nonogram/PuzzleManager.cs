50f84786a24d4ec8439b4c306142cd9e0db23fd4
eLilyPad <elizabethbryce00@gmail.com>
2026-01-02 19:45:00 +0000

    - stuff

--- Nonogram/PuzzleManager.cs
+++ Nonogram/PuzzleManager.cs
@@ -7,10 +7,9 @@ using static PuzzleData;
 using static Display;
 public sealed class PuzzleManager
 {
-	public sealed record class Settings
+	public readonly record struct Settings(bool LineCompleteBlockRest, bool HaveTimer)
 	{
-		public bool LineCompleteBlockRest { get; set; } = true;
-		public bool HaveTimer { get; set; } = true;
+
 	}
 	public sealed record class CurrentPuzzle : Hints.IProvider, Tiles.IProvider
 	{
@@ -24,31 +23,10 @@ public sealed class PuzzleManager
 				if (value is null) { return; }
 				Display display = UI.Display;
 				Instance.Puzzles[value.Name] = field = value;
-				IEnumerable<HintPosition> hintValues = HintPosition.AsRange(display.TilesGrid.Columns = value.Size);
-				IEnumerable<Vector2I> tileValues = (Vector2I.One * value.Size).GridRange();
-				for (int i = 0; i < tileValues.Count(); i++)
-				{
-					Vector2I position = tileValues.ElementAt(i);
-					Tile tile = _tiles.GetOrCreate(position);
-					TileMode state = value.States.GetValueOrDefault(key: position, defaultValue: TileMode.NULL);
-					_tiles.ChangeMode(position, tile, input: state);
+				Timer.Elapsed = field.TimeTaken;
 
-					if (i == 0) _hints.TileSize = tile.Size;
-				}
-				foreach (HintPosition position in hintValues)
-				{
-					Hint hint = _hints.GetOrCreate(position);
-					_hints.ApplyText(position, hint);
-				}
-				_tiles.Clear(exceptions: tileValues);
-				_hints.Clear(exceptions: hintValues);
-
-				Timer.Elapsed = value.TimeTaken;
-
-				float scale = value.Size * value.Size / value.Size;
-				display.TilesGrid.CustomMinimumSize = Mathf.CeilToInt(scale) * _hints.TileSize;
-				display.ResetTheme();
-
+				display.UpdateView(field, _tiles, _hints);
+				display.TilesGrid.CustomMinimumSize = Mathf.CeilToInt(field.Size) * _hints.TileSize;
 			}
 		} = new SaveData();
 
@@ -109,6 +87,7 @@ public sealed class PuzzleManager
 			{
 				case Type.Game:
 					ChangeState(position, mode: input, tile);
+					input.PlayAudio();
 					if (Settings.LineCompleteBlockRest)
 					{
 						foreach (Side side in stackalloc[] { Side.Row, Side.Column })
@@ -121,10 +100,12 @@ public sealed class PuzzleManager
 							}
 						}
 					}
-					if (Settings.HaveTimer && input is TileMode.Filled && !Timer.Running) Timer.Running = true;
-					input.PlayAudio();
+					if (Settings.HaveTimer && input is TileMode.Filled && !Timer.Running)
+					{
+						Timer.Running = true;
+					}
 					Save(Puzzle);
-					if (Puzzle is { IsComplete: true })
+					if (Puzzle.IsComplete)
 					{
 						//Instance.PuzzlesCompleted[PuzzleManager.Current.Puzzle.Name] = true;
 						UI.CompletionScreen.Show();
@@ -132,7 +113,6 @@ public sealed class PuzzleManager
 						{
 							Dialogues.Start(dialogueName, true);
 						}
-
 					}
 					break;
 				case Type.Paint:
@@ -182,14 +162,7 @@ public sealed class PuzzleManager
 	public List<Pack> PuzzlePacks { get; } = [Pack.Procedural()];
 	public Dictionary<string, bool> PuzzlesCompleted { private get; init; } = [];
 	public Dictionary<string, string> CompletionDialogues { private get; init; } = [];
-	public Dictionary<string, Data> Puzzles { private get; init; } = new()
-	{
-		[Data.DefaultName] = new PuzzleData()
-	};
+	public Dictionary<string, Data> Puzzles { private get; init; } = new() { [Data.DefaultName] = new PuzzleData() };
 
 	private PuzzleManager() { }
-	public void AddPuzzle(SaveData save)
-	{
-		Puzzles[save.Name] = save;
-	}
 }
