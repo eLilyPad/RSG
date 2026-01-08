f310bd289851f118f78f940c6f0440837b880cac
eLilyPad <elizabethbryce00@gmail.com>
2026-01-08 01:21:26 +0000

    - added a settings check to see if it can be enabled

--- Nonogram/PuzzleTimer.cs
+++ Nonogram/PuzzleTimer.cs
@@ -3,6 +3,7 @@ namespace RSG.Nonogram;
 public sealed class PuzzleTimer
 {
 	public required Action<string> TimeChanged { get; init; }
+	public required Settings Settings { private get; set; }
 	public TimeSpan Elapsed
 	{
 		get; set
@@ -11,7 +12,18 @@ public sealed class PuzzleTimer
 			TimeChanged($"{field.TotalHours:00}:{field.Minutes:00}:{field.Seconds:00}");
 		}
 	}
-	public bool Running { get; set; } = false;
+	public bool Running
+	{
+		get => field; set
+		{
+			if (!Settings.HaveTimer)
+			{
+				field = false;
+				return;
+			}
+			field = value;
+		}
+	} = false;
 	public void Tick(double delta)
 	{
 		if (!Running) return;
