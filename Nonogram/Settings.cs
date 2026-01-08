32c55b1f038d0c904eb3c603d934dd04d82ddc54
eLilyPad <elizabethbryce00@gmail.com>
2026-01-08 01:19:29 +0000

    - added settings for nonogram game mode

--- Nonogram/Settings.cs
+++ Nonogram/Settings.cs
@@ -1,8 +1,11 @@
-namespace RSG.Nonogram;
-
-public sealed record class Settings
+namespace RSG.Nonogram
 {
-	public bool LineCompleteLock { get; set; } = true;
-	public bool LineCompleteBlockRest { get; set; } = true;
-	public bool HaveTimer { get; set; } = true;
+public readonly record struct Settings()
+{
+	public bool LockCompletedFilledTiles { get; init; } = true;
+	public bool LockCompletedBlockTiles { get; init; } = true;
+	public bool LineCompleteBlockRest { get; init; } = true;
+	public bool ShowMistakes { get; init; } = true;
+	public bool HaveTimer { get; init; } = true;
+}
 }
