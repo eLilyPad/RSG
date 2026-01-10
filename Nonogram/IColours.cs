c8931ace9a41b138326c9c8327a9d53500fa5c84
eLilyPad <elizabethbryce00@gmail.com>
2026-01-10 15:34:39 +0000

    - added functions to get colours based on TileMode

--- Nonogram/IColours.cs
+++ Nonogram/IColours.cs
@@ -2,6 +2,8 @@ using Godot;
 
 namespace RSG.Nonogram;
 
+using Mode = Display.TileMode;
+
 public interface IColours
 {
 	Color NonogramBackground { get; }
@@ -13,4 +15,21 @@ public interface IColours
 	Color NonogramTileBackground2 { get; }
 	Color NonogramTileBackground1 { get; }
 	Color NonogramTileBackgroundFilled { get; }
+
+	Color NonogramTileBackground(Mode mode, bool alternative)
+	{
+		Color
+		filled = NonogramTileBackgroundFilled,
+		background = alternative ? NonogramTileBackground1 : NonogramTileBackground2,
+		blocked = background.Darkened(.2f);
+
+		filled = alternative ? filled : filled.Darkened(.2f);
+
+		return mode switch { Mode.Filled => filled, Mode.Blocked => blocked, _ => background };
+	}
+	Color NonogramLockedBorder(Mode mode) => mode switch
+	{
+		Mode.Filled => NonogramFilledBorder,
+		_ => NonogramBlockedBorder
+	};
 }
