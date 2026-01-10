cd4b32d0f6c88de25cbcbaf5d9222abf4fc8ad42
eLilyPad <elizabethbryce00@gmail.com>
2026-01-10 21:04:21 +0000

    - simplified methods to work on Images and not the texture rect that uses it

--- UI/ImageCreationExtension.cs
+++ UI/ImageCreationExtension.cs
@@ -4,23 +4,32 @@ namespace RSG.UI;
 
 public static class ImageCreationExtension
 {
-	public static void DrawLines(this TextureRect rect, Vector2I size, float space = 173f)
+	public static void TextureLines(this TextureRect rect, Vector2I size, float space = 173f)
 	{
-		const int shift = 2, lineThickness = 4;
-		Image image = Image.CreateEmpty(size.X, size.Y, false, Image.Format.Rgba8);
-		foreach ((int x, int y) in size.GridRange())
-		{
-			int shiftedX = x - shift, shiftedY = y - shift;
-			if (x % space is not 0 && y % space is not 0) continue;
-			if (x < lineThickness || y < lineThickness) continue;
-			image.SetPixel(shiftedX, shiftedY, Colors.DarkGray, lineThickness);
-		}
+		Image image = Image
+			.CreateEmpty(size.X, size.Y, false, Image.Format.Rgba8)
+			.TextureLines(space);
 		rect.Texture = ImageTexture.CreateFromImage(image);
 	}
-	public static void DrawBorder(this TextureRect rect, Vector2I size, int pixelSize = 5, Color? borderColour = null)
+	public static void TextureBorder(this TextureRect rect, Vector2I size, int pixelSize = 5, Color? borderColour = null)
+	{
+		Image image = Image
+			.CreateEmpty(size.X, size.Y, false, Image.Format.Rgba8)
+			.TextureBorder(pixelSize, borderColour);
+		rect.Texture = ImageTexture.CreateFromImage(image);
+	}
+	public static void TextureNoise(this TextureRect rect, Vector2I size, Func<float, Color> colour)
+	{
+		Image image = Image
+			.CreateEmpty(size.X, size.Y, false, Image.Format.Rgba8)
+			.TextureNoise(colour);
+		rect.Texture = ImageTexture.CreateFromImage(image);
+	}
+
+	private static Image TextureBorder(this Image image, int pixelSize = 5, Color? borderColour = null)
 	{
 		Color color = borderColour ?? Colors.DarkGray;
-		Image image = Image.CreateEmpty(size.X, size.Y, false, Image.Format.Rgba8);
+		Vector2I size = image.GetSize();
 		foreach (Vector2I position in size.GridRange())
 		{
 			(int x, int y) = position;
@@ -31,6 +40,36 @@ public static class ImageCreationExtension
 				image.SetPixel(x, y, color, pixelSize * pixelSize);
 			}
 		}
-		rect.Texture = ImageTexture.CreateFromImage(image);
+		return image;
+	}
+	private static Image TextureLines(this Image image, float space = 173f)
+	{
+		const int shift = 2, lineThickness = 4;
+		Vector2I size = image.GetSize();
+		foreach ((int x, int y) in size.GridRange())
+		{
+			int shiftedX = x - shift, shiftedY = y - shift;
+			if (x % space is not 0 && y % space is not 0) continue;
+			if (x < lineThickness || y < lineThickness) continue;
+			image.SetPixel(shiftedX, shiftedY, Colors.DarkGray, lineThickness);
+		}
+		return image;
+	}
+	private static Image TextureNoise(this Image image, Func<float, Color> colour)
+	{
+		const float threshold = .1f;
+
+		FastNoiseLite noise = new()
+		{
+			FractalOctaves = 10,
+			Seed = Random.Shared.Next(5),
+		};
+		foreach (Vector2I position in image.GetSize().GridRange())
+		{
+			(int x, int y) = position;
+			float value = noise.GetNoise2Dv(position);
+			image.SetPixel(x, y, colour(value), 1);
+		}
+		return image;
 	}
 }
