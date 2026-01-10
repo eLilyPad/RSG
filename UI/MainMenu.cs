078499d7629eee24b2022287ed765dfa4ccb024b
eLilyPad <elizabethbryce00@gmail.com>
2026-01-10 21:03:11 +0000

    - use custom button class for menu buttons

--- UI/MainMenu.cs
+++ UI/MainMenu.cs
@@ -17,13 +17,62 @@ public sealed partial class MainMenu : Container
 			.Preset(LayoutPreset.FullRect, LayoutPresetMode.KeepWidth, Margin);
 		public override void _Ready() => this.Add(Audio, Video, Input, Nonogram);
 	}
+	private sealed partial class MainButton : Button
+	{
+		public TextureRect Background { get; } = new TextureRect { Name = "Background", }
+			.Preset(LayoutPreset.FullRect);
+		public RichTextLabel Label { get; } = new RichTextLabel
+		{
+			Name = "Label",
+			SelectionEnabled = false,
+			FitContent = true,
+			BbcodeEnabled = true,
+			MouseFilter = MouseFilterEnum.Ignore
+		}.Preset(LayoutPreset.FullRect);
+		public MainButton(string name)
+		{
+			Name = name;
+			Label.PushColor(Colors.Black);
+			Label.AddText(name);
+			Text = name;
+		}
+		public override void _Ready()
+		{
+			this
+				.Add(Background, Label)
+				.SizeFlags(SizeFlags.Fill, SizeFlags.Fill)
+				.AddAllFontThemeOverride(Colors.Transparent)
+				.OverrideStyle(modify: (StyleBoxFlat style) =>
+				{
+					style.CornerDetail = 1;
+					style.BgColor = Colors.Transparent;
+					//style.ExpandMarginRight = 100;
+					return style;
+				});
+			Label
+				.OverrideStyle(modify: (StyleBoxFlat style) =>
+				{
+					style.CornerDetail = 1;
+					//style.BgColor = Colors.Transparent;
+					//style.ExpandMarginRight = 100;
+					return style;
+				})
+				.AddThemeFontSizeOverride("normal", 40);
+			Resized += () => Background.TextureNoise((Vector2I)Size, value => value switch
+			{
+				< .1f => Colors.DarkGray,
+				> .6f => Colors.DarkSlateGray,
+				_ => Colors.Transparent
+			});
+		}
+	}
 	public sealed partial class MainButtons : VBoxContainer
 	{
-		public Button Play { get; } = new() { Name = nameof(Play), Text = nameof(Play) };
-		public Button Levels { get; } = new() { Name = nameof(Levels), Text = nameof(Levels) };
-		public Button Dialogues { get; } = new() { Name = nameof(Dialogues), Text = nameof(Dialogues) };
-		public Button Settings { get; } = new() { Name = nameof(Settings), Text = nameof(Settings) };
-		public Button Quit { get; } = new() { Name = nameof(Quit), Text = nameof(Quit) };
+		public BaseButton Play { get; } = new MainButton(nameof(Play));
+		public BaseButton Levels { get; } = new MainButton(nameof(Levels));
+		public BaseButton Dialogues { get; } = new MainButton(nameof(Dialogues));
+		public BaseButton Settings { get; } = new MainButton(nameof(Settings));
+		public BaseButton Quit { get; } = new MainButton(nameof(Quit));
 		public override void _Ready() => this.Add(Play, Levels, Dialogues, Settings, Quit);
 	}
 
@@ -34,7 +83,8 @@ public sealed partial class MainMenu : Container
 	public SettingsContainer Settings { get; } = new SettingsContainer { Name = "Settings", Visible = false }
 		.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize, Margin);
 	public MainButtons Buttons { get; } = new MainButtons { Name = nameof(Buttons) }
-		.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize, Margin);
+		//.SizeFlags(SizeFlags.Fill, SizeFlags.Fill)
+		.Preset(preset: LayoutPreset.BottomLeft, resizeMode: LayoutPresetMode.KeepSize, Margin);
 	public Nonogram.PuzzleSelector Levels { get; } = new Nonogram.PuzzleSelector { Name = "Level Selector", Visible = false }
 		.Preset(preset: LayoutPreset.FullRect, resizeMode: LayoutPresetMode.KeepSize, Margin);
 	public DialogueSelector Dialogues { get; } = new DialogueSelector { Name = "Dialogue Selector", Visible = false }
