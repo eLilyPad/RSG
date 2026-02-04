bf719cb847a09c3145cc788d36a0d732fc0165aa
eLilyPad <elizabethbryce00@gmail.com>
2026-02-04 22:22:50 +0000

    made class for Theme extensions

---
+++ Nonogram/ThemeExtensions.cs
@@ -0,0 +1,19 @@
+namespace RSG.Nonogram;
+
+public static class ThemeExtensions
+{
+	public static void ResetTheme(this Display display)
+	{
+		const int marginValue = 100, spacerValue = 1;
+		display.Grid.AddThemeConstantOverride("h_separation", 1);
+		display.Grid.AddThemeConstantOverride("v_separation", 1);
+		display.Rows.AddThemeConstantOverride("separation", spacerValue);
+		display.Columns.AddThemeConstantOverride("separation", spacerValue);
+		display.Margin.AddThemeConstantOverride("margin_top", marginValue);
+		display.Margin.AddThemeConstantOverride("margin_bottom", marginValue / 2);
+		display.TilesGrid.AddThemeConstantOverride("h_separation", 0);
+		display.TilesGrid.AddThemeConstantOverride("v_separation", 0);
+		display.TilesGrid.AddThemeConstantOverride("h_separation", spacerValue);
+		display.TilesGrid.AddThemeConstantOverride("v_separation", spacerValue);
+	}
+}
