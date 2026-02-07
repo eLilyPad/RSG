b9c559d9a039a607ca83ccff634590b50b351f7e
eLilyPad <elizabethbryce00@gmail.com>
2026-02-07 19:33:18 +0000

    added container interface

--- Nonogram/Settings.cs
+++ Nonogram/Settings.cs
@@ -2,6 +2,7 @@ namespace RSG.Nonogram;
 
 public readonly record struct Settings()
 {
+	public interface IHave { Settings Settings { get; set; } }
 	public bool LockCompletedFilledTiles { get; init; } = true;
 	public bool LockCompletedBlockedTiles { get; init; } = true;
 	public bool LineCompleteBlockRest { get; init; } = true;
