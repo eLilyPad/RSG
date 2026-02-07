013e37203ec92ad07aefb54ebb54c1b008ebbb91
eLilyPad <elizabethbryce00@gmail.com>
2026-02-07 19:25:32 +0000

    modified node pool

--- NodePool.cs
+++ NodePool.cs
@@ -2,10 +2,68 @@ using Godot;
 
 namespace RSG;
 
-public abstract class NodePool<TKey, TValue> where TKey : notnull where TValue : Node
+public static class PoolExtensions
 {
+	public static void Update<TKey, TValue, TConfig, TPool>(this TPool pool, TConfig config, IEnumerable<TKey> keys)
+	where TKey : notnull
+	where TValue : Node
+	where TPool : NodePool<TKey, TValue>, Nonogram.IRefresh<TValue, TConfig>, Nonogram.Tile.ISize
+	{
+		foreach ((TKey _, TValue value) in pool)
+		{
+			pool.Refresh(value, config);
+		}
+		pool.Clear(exceptions: keys);
+	}
+	public static void Refresh<TKey, TValue, TConfig, TPool>(this TPool pool, TConfig config)
+	where TKey : notnull
+	where TValue : Control
+	where TPool : NodePool<TKey, TValue>, Nonogram.IRefresh<TValue, TConfig>, Nonogram.Tile.ISize
+	{
+		foreach ((TKey _, TValue value) in pool)
+		{
+			pool.Refresh(value, config);
+			value.CustomMinimumSize = pool.TileSize;
+		}
+	}
+
+}
+
+public abstract class NodePool<TKey, TValue, TConfig> : NodePool<TKey, TValue> where TKey : notnull where TValue : Node
+{
+	public TValue GetOrCreate(TKey key, TConfig config)
+	{
+		if (!_nodes.TryGetValue(key, out TValue? value))
+		{
+			return _nodes[key] = Create(key, config);
+		}
+		return value;
+	}
+	protected abstract TValue Create(TKey key, TConfig config);
+	protected abstract Node Parent(TKey key, TConfig config);
+}
+public abstract class NodePool<TKey, TValue> : IEnumerable<KeyValuePair<TKey, TValue>>
+where TKey : notnull
+where TValue : Node
+{
+	public IEnumerable<TKey> Keys => [.. _nodes.Keys];
 	protected readonly Dictionary<TKey, TValue> _nodes = [];
-	public TValue GetOrCreate(TKey key) => _nodes.GetOrCreate(key, create: Create);
+	public void ReplaceAll(IEnumerable<TKey> keys)
+	{
+		foreach (TKey key in keys)
+		{
+			_nodes[key] = Create(key);
+		}
+		Clear(exceptions: keys);
+	}
+	public TValue GetOrCreate(TKey key)
+	{
+		if (!_nodes.TryGetValue(key, out TValue? value))
+		{
+			return _nodes[key] = Create(key);
+		}
+		return value;
+	}
 	public void Clear(IEnumerable<TKey> exceptions)
 	{
 		foreach ((TKey key, TValue node) in _nodes)
@@ -15,7 +73,10 @@ public abstract class NodePool<TKey, TValue> where TKey : notnull where TValue :
 			_nodes.Remove(key);
 		}
 	}
-
 	protected abstract TValue Create(TKey key);
 	protected abstract Node Parent(TKey key);
+
+	IEnumerator<KeyValuePair<TKey, TValue>> IEnumerable<KeyValuePair<TKey, TValue>>.GetEnumerator() => _nodes.GetEnumerator();
+
+	IEnumerator IEnumerable.GetEnumerator() => _nodes.GetEnumerator();
 }
