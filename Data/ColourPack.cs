f7d3964f3fa1f026613967af60da64dccae399fc
eLilyPad <elizabethbryce00@gmail.com>
2026-02-12 13:28:44 +0000

    added some colour func

--- Data/ColourPack.cs
+++ Data/ColourPack.cs
@@ -2,11 +2,66 @@ using Godot;
 
 namespace RSG;
 
+using Nonogram;
+using Minesweeper;
+
+using Mode = Nonogram.Display.TileMode;
+
 public static class ColoursPackExtensions
 {
+	public static TCore SetColours<TCore, TNonogram>(this TCore config)
+	where TCore : IHaveColours<Nonogram.IColours>, IHaveCurrent<TNonogram>
+	where TNonogram : NonogramContainer.IHave, IAlternate, SaveData.IHave, IDisplayType, ITiles<NodePool<Vector2I, Nonogram.Tile, TNonogram>, TNonogram>
+	{
+		config.Nonogram.SetColours(config.Colours);
+		return config;
+	}
+	public static TCurrent SetColours<TCurrent>(this TCurrent config, Nonogram.IColours? value = null)
+	where TCurrent :
+		NonogramContainer.IHave,
+		SaveData.IHave,
+		IAlternate,
+		IDisplayType,
+		ITiles<NodePool<Vector2I, Nonogram.Tile, TCurrent>, TCurrent>
+	{
+		value ??= ColourPack.Default;
+		config.UI.Background.ColorBackground.Color = config.Type.Background(value);
+		config.UI.Display.Spacer.Timer.Background.Color = value.NonogramTimerBackground;
+		foreach ((Vector2I position, Nonogram.Tile tile) in config.Tiles)
+		{
+			tile.SetColours(position, config, value);
+		}
+		return config;
+	}
+	public static Nonogram.Tile SetColours<T>(this Nonogram.Tile tile, Vector2I position, T config, Nonogram.IColours? value = null)
+	where T : SaveData.IHave, IAlternate
+	{
+		Mode mode = config.Puzzle.States.GetValueOrDefault(position);
+		return tile.SetColours(mode, config.IsAlternative(position), value);
+	}
+	public static Nonogram.Tile SetColours(this Nonogram.Tile tile, Mode mode, bool isAlternative, Nonogram.IColours? value = null)
+	{
+		value ??= ColourPack.Default;
 
+		//mode ??= tile.Mode;
+		Color tileColour = value.NonogramTileBackground(mode, alternative: isAlternative);
+		Color lockedColour = value.NonogramLockedBorder(mode);
+		tile.Button.OverrideStyle(modify: (StyleBoxFlat style) =>
+		{
+			style.BorderColor = lockedColour;
+			style.BgColor = tileColour;
+			return style;
+		});
+		tile.Button.OverrideStyle(name: "hover", modify: (StyleBoxFlat style) =>
+		{
+			style.BgColor = tileColour;
+			return style;
+		});
+		return tile;
+	}
 }
 
+public interface IHaveColours<T> { T Colours { get; } }
 public sealed partial class ColourPack : Resource, Nonogram.IColours, Minesweeper.IColours
 {
 	public static ColourPack Default { get; } = new ColourPack();
@@ -16,6 +71,7 @@ public sealed partial class ColourPack : Resource, Nonogram.IColours, Minesweepe
 	[Export] public Color MainMenuDialoguesBackground { get; private set; } = Colors.DimGray;
 
 	[Export] public Color NonogramBackground { get; private set; } = Colors.DarkOliveGreen;
+	[Export] public Color NonogramPaintBackground { get; private set; } = Colors.Aqua;
 	[Export] public Color NonogramFilledBorder { get; private set; } = Colors.DarkBlue;
 	[Export] public Color NonogramBlockedBorder { get; private set; } = Colors.DarkSeaGreen;
 	[Export] public Color NonogramTimerBackground { get; private set; } = Colors.Burlywood;
