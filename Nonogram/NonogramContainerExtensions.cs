d3bbecd0ab6aff740a99eaba71f9fdaa38e6c85b
eLilyPad <elizabethbryce00@gmail.com>
2026-02-13 01:52:36 +0000

    comment out unused

--- Nonogram/NonogramContainerExtensions.cs
+++ Nonogram/NonogramContainerExtensions.cs
@@ -29,23 +29,23 @@ public static class NonogramContainerExtensions
 		config.UI.CompletionScreen.Value.Signals = events;
 		return config;
 	}
-	public static PuzzleType ChangeType<T>(this T a, ref PuzzleType field, PuzzleType value) where T : IHave
-	{
-		if (field == value) return field;
-		field = value;
-		Display display = a.UI.Display;
-		Display.DisplaySpacer spacer = display.Spacer;
-		display.Name = field.AsName();
-		bool displayingTimer = spacer.HasChild(spacer.Timer);
-		switch (field)
-		{
-			case PuzzleType.Game when !displayingTimer:
-				spacer.AddChild(spacer.Timer);
-				break;
-			case PuzzleType.Paint or PuzzleType.Display when displayingTimer:
-				spacer.RemoveChild(spacer.Timer);
-				break;
-		}
-		return field;
-	}
+	//public static PuzzleType ChangeType<T>(this T a, ref PuzzleType field, PuzzleType value) where T : IHave
+	//{
+	//	if (field == value) return field;
+	//	field = value;
+	//	Display display = a.UI.Display;
+	//	Display.DisplaySpacer spacer = display.Spacer;
+	//	display.Name = field.AsName();
+	//	bool displayingTimer = spacer.HasChild(spacer.Timer);
+	//	switch (field)
+	//	{
+	//		case PuzzleType.Game when !displayingTimer:
+	//			spacer.AddChild(spacer.Timer);
+	//			break;
+	//		case PuzzleType.Paint or PuzzleType.Display when displayingTimer:
+	//			spacer.RemoveChild(spacer.Timer);
+	//			break;
+	//	}
+	//	return field;
+	//}
 }
