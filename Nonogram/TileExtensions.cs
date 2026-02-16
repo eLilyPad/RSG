bc2d666bc0772ba59b64aeed406cdaf045ba4b92
eLilyPad <elizabethbryce00@gmail.com>
2026-02-16 15:23:48 +0000

    removed unused

--- Nonogram/TileExtensions.cs
+++ Nonogram/TileExtensions.cs
@@ -6,9 +6,6 @@ using static Display;
 
 public static class TileExtensions
 {
-	public static bool ShouldIgnore(this TileMode expected, TileMode current, TileMode newValue) =>
-		expected == current
-		&& !(newValue is TileMode.Blocked && current is TileMode.Clear);
 	public static bool IsCorrect<TKey>(this IImmutableDictionary<TKey, TileMode> tiles, TKey position, TileMode current)
 	{
 		if (!tiles.TryGetValue(position, out TileMode expected)) return false;
@@ -20,32 +17,6 @@ public static class TileExtensions
 		TileMode.Clear when current is TileMode.Clear or TileMode.Blocked => true,
 		_ => false
 	};
-
-	public static double ToDouble(this TileMode mode) => mode switch
-	{
-		TileMode.Blocked => 2,
-		TileMode.Filled => 1,
-		_ => 0,
-	};
-	public static TileMode Change(this TileMode input, TileMode currents) => input switch
-	{
-		TileMode.NULL => currents,
-		TileMode mode when mode == currents => TileMode.Clear,
-		TileMode mode => mode
-	};
-	public static TileMode ToTileMode(this int mode) => mode switch
-	{
-		2 => TileMode.Blocked,
-		1 => TileMode.Filled,
-		_ => 0,
-	};
-	public static TileMode FromText(this string mode) => mode switch
-	{
-		BlockText => TileMode.Blocked,
-		FillText => TileMode.Filled,
-		EmptyText => TileMode.Clear,
-		_ => TileMode.NULL
-	};
 	public static void PlayAudio(this TileMode mode)
 	{
 		if (mode.AsAudioStream() is AudioStream stream) Audio.Buses.SoundEffects.Play(stream);
@@ -56,10 +27,6 @@ public static class TileExtensions
 		TileMode.Blocked => Audio.NonogramSounds.BlockTileClicked,
 		_ => null
 	};
-	public static string AsText<T>(this IImmutableDictionary<T, TileMode> modes, T position) where T : notnull
-	{
-		return modes.GetValueOrDefault(position, TileMode.Clear).AsText();
-	}
 	public static string AsText(this TileMode mode) => mode switch
 	{
 		TileMode.Blocked => BlockText,
