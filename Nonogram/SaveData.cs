bee52cd5aab94c259f8e50cec6cfb5b94cd6f359
eLilyPad <elizabethbryce00@gmail.com>
2026-02-16 17:19:53 +0000

    yay

--- Nonogram/SaveData.cs
+++ Nonogram/SaveData.cs
@@ -1,103 +1,12 @@
-using static System.Text.Json.JsonSerializer;
 using System.Text.Json.Serialization;
-using System.Text.Json;
 using Godot;
 
 namespace RSG.Nonogram;
 
 using Mode = Display.TileMode;
 
-public sealed record SaveData : Display.Data
+public sealed partial record SaveData : Display.Data
 {
-	public sealed class Converter : JsonConverter<SaveData>
-	{
-		public const string ExpectedProp = "Expected";
-
-		public override SaveData? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-		{
-			if (reader.TokenType != JsonTokenType.StartObject)
-			{
-				GD.PrintErr("SaveData root is not an object");
-				return null;
-			}
-			PuzzleData? expected = null;
-			Dictionary<Vector2I, Mode>? tiles = null;
-			TimeSpan timeTaken = TimeSpan.Zero;
-
-			while (reader.Read())
-			{
-				if (reader.TokenType == JsonTokenType.EndObject) break;
-
-				if (reader.TokenType != JsonTokenType.PropertyName) throw new JsonException();
-
-				string prop = reader.GetString()!;
-				reader.Read();
-
-				switch (prop)
-				{
-					case ExpectedProp:
-						expected = Deserialize<PuzzleData>(ref reader, options);
-						break;
-					case PropertyNames.Tiles:
-						tiles = Deserialize<Dictionary<Vector2I, Mode>>(ref reader, options);
-						break;
-					case PropertyNames.TimeTaken:
-						timeTaken = ReadTimeSpan(ref reader, options);
-						break;
-					default:
-						reader.Skip();
-						break;
-				}
-			}
-
-			if (expected is null)
-			{
-				GD.PrintErr($"Missing property in JSON: {ExpectedProp}");
-				return null;
-			}
-			if (tiles is null) return null;
-
-			return new SaveData
-			{
-				Name = expected.Name,
-				Expected = expected,
-				Tiles = tiles,
-				TimeTaken = timeTaken
-			};
-		}
-		public override void Write(Utf8JsonWriter writer, SaveData value, JsonSerializerOptions options)
-		{
-			writer.WriteStartObject();
-			writer.WritePropertyName(ExpectedProp);
-			Serialize(writer, value.Expected, options);
-			writer.WritePropertyName(PropertyNames.TimeTaken);
-			Serialize(writer, value.TimeTaken, options);
-			writer.WritePropertyName(PropertyNames.Tiles);
-			Serialize(writer, value.Tiles, options);
-			writer.WriteEndObject();
-		}
-		private static TimeSpan ReadTimeSpan(ref Utf8JsonReader reader, JsonSerializerOptions options)
-		{
-			try
-			{
-				return reader.TokenType switch
-				{
-					JsonTokenType.String =>
-						Deserialize<TimeSpan>(ref reader, options),
-
-					JsonTokenType.Number =>
-						TimeSpan.FromSeconds(reader.GetDouble()),
-
-					_ => TimeSpan.Zero
-				};
-			}
-			catch (Exception e)
-			{
-				GD.PrintErr($"Invalid TimeTaken value: {e.Message}");
-				return TimeSpan.Zero;
-			}
-		}
-	}
 	internal readonly record struct InputEvent(
 		Vector2I Position,
 		Settings Settings,
@@ -127,17 +36,17 @@ public sealed record SaveData : Display.Data
 		if (tile.Locked) return;
 		mode.PlayAudio();
 		ChangeState(position, mode: tile.Mode = mode);
-		BlockCompletedLines(tiles, position, settings);
+
+		if (settings.LineCompleteBlockRest)
+		{
+			BlockCompletedLine(side: Display.Side.Row);
+			BlockCompletedLine(side: Display.Side.Column);
+		}
 
 		if (tiles.LockRules.ShouldLock(position)) tile.Locked = true;
 		if (!timer.Running && mode is Mode.Filled) timer.Running = true;
 		if (IsComplete) eventHandler?.Completed(this);
-	}
-	private void BlockCompletedLines(Tile.Pool tiles, Vector2I position, Settings settings)
-	{
-		if (!settings.LineCompleteBlockRest) return;
-		BlockCompletedLine(side: Display.Side.Row);
-		BlockCompletedLine(side: Display.Side.Column);
+
 		void BlockCompletedLine(Display.Side side)
 		{
 			if (!IsLineComplete(position, side)) { return; }
