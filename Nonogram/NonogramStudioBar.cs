e513e34db95dc78bfad09f3abbb2e1367ed964f4
eLilyPad <elizabethbryce00@gmail.com>
2026-02-17 23:26:49 +0000

    added signal connections

--- Nonogram/NonogramStudioBar.cs
+++ Nonogram/NonogramStudioBar.cs
@@ -2,13 +2,37 @@ using Godot;
 
 namespace RSG.Nonogram;
 
+public interface IChangePuzzle
+{
+	void ModifyName(string value);
+	void ModifySize(double value);
+}
 public sealed partial class NonogramStudioBar : Container
 {
 	public sealed partial class PuzzleTabContainer : VBoxContainer
 	{
 		public LineEdit EditableName { get; } = new LineEdit { Text = "Name" };
-		public SpinBox PuzzleSize { get; } = new SpinBox { Name = "Puzzle Size" };
-		public override void _Ready() => this.Add(EditableName);
+		public SpinBox PuzzleSize { get; } = new SpinBox
+		{
+			Name = "Puzzle Size",
+			MinValue = 10,
+			AllowLesser = false
+		};
+		public IChangePuzzle Signals
+		{
+			set
+			{
+				if (field is not null)
+				{
+					EditableName.TextChanged -= field.ModifyName;
+					PuzzleSize.ValueChanged -= field.ModifySize;
+				}
+				field = value;
+				EditableName.TextChanged += field.ModifyName;
+				PuzzleSize.ValueChanged += field.ModifySize;
+			}
+		}
+		public override void _Ready() => this.Add(EditableName, PuzzleSize);
 	}
 	public ColorRect Background { get; } = new ColorRect { Name = "Background", Color = Colors.AliceBlue }
 		.Preset(LayoutPreset.FullRect);
