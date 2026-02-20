ebc37f4206435cfe9c837b27518dd6d02b80c825
eLilyPad <elizabethbryce00@gmail.com>
2026-02-20 14:38:41 +0000

    moved func and added gaurd for TileMode.NULL

--- Nonogram/TileExtensions.cs
+++ Nonogram/TileExtensions.cs
@@ -17,14 +17,15 @@ public static class TileExtensions
 		TileMode.Clear when current is TileMode.Clear or TileMode.Blocked => true,
 		_ => false
 	};
+	public static bool IsValidInput(this TileMode current, ref TileMode input)
+	{
+		if (input is TileMode.NULL) return false;
+		input = input == current ? TileMode.Clear : input;
+		return !TileMode.Clear.AllEqual(current, input);
+	}
 	public static void PlayAudio(this TileMode mode)
 	{
 		if (mode.AsAudioStream() is AudioStream stream) Audio.Buses.SoundEffects.Play(stream);
-	}
-	public static bool IsValidInput(this TileMode current, ref TileMode next)
-	{
-		next = next == current ? TileMode.Clear : next;
-		return !TileMode.Clear.AllEqual(current, next);
 	}
 	public static AudioStream? AsAudioStream(this TileMode mode) => mode switch
 	{
