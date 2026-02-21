cb54cf00e571c00cd6c3726d1f51cc36388b6d9b
eLilyPad <elizabethbryce00@gmail.com>
2026-02-21 18:29:04 +0000

    refactored

--- Nonogram/NonogramContainer.cs
+++ Nonogram/NonogramContainer.cs
@@ -54,11 +54,6 @@ public sealed partial class NonogramContainer : PanelContainer
 		Tiles = tiles;
 		Hints = hints;
 	}
-	//internal NonogramContainer(List<Func<Vector2I, bool>> rules, PuzzleManager.CurrentPuzzle puzzle)
-	//{
-	//	Hints = new(Provider: puzzle);
-	//	Tiles = new(Provider: puzzle) { LockRules = new() { Rules = rules } };
-	//}
 	public override void _Ready() => this.Add(Background, Container.Add(Display, Studio), CompletionScreen);
 
 }
