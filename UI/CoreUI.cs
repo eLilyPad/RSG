46896a12617f7faed5b386c926b5ca2b56ab9ceb
eLilyPad <elizabethbryce00@gmail.com>
2026-02-21 14:49:10 +0000

    dont show buttons and dialogue silly billy

--- UI/CoreUI.cs
+++ UI/CoreUI.cs
@@ -27,7 +27,7 @@ public sealed partial class CoreUI : Control
 			PuzzleManager.CurrentPuzzle current = PuzzleManager.Current;
 			Dialogues.Start(name: current.CompletionDialogueName);
 			UI.Menu.Show();
-			UI.Menu.Buttons.Show();
+			UI.Menu.Buttons.Hide();
 			current.UI.CompletionScreen.Hide();
 		}
 		void PuzzleCompleteScreen.IHandleSignals.OnVisibilityChanged()
