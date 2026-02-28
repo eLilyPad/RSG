441c900f690cca34cf4fb71b39ca8a4509456e83
eLilyPad <elizabethbryce00@gmail.com>
2026-02-28 22:36:21 +0000

    added named ordering Func

--- Vector2IExtensions.cs
+++ Vector2IExtensions.cs
@@ -12,11 +12,29 @@ public static class Vector2IExtensions
 	{
 		return value.Where(pair => indexer(pair.Key) == index);
 	}
+	public static IOrderedEnumerable<KeyValuePair<Vector2I, T>> OrderBy<T>(
+		this IEnumerable<KeyValuePair<Vector2I, T>> value,
+		Func<Vector2I, int> selector
+	)
+	{
+		return value.OrderBy(pair => selector(pair.Key));
+	}
 	public static IEnumerable<KeyValuePair<Vector2I, T>> AllInLines<T>(
 		this IEnumerable<KeyValuePair<Vector2I, T>> tiles,
 		Vector2I position
 	)
 	{
 		return tiles.Where(pair => pair.Key.EitherEqual(position));
+	}
+	public static IOrderedEnumerable<KeyValuePair<Vector2I, T>> InOrderedLine<T>(
+		this IEnumerable<KeyValuePair<Vector2I, T>> tiles,
+		Vector2I position,
+		Func<Vector2I, int> selector,
+		Func<Vector2I, int> indexer
+	)
+	{
+		return tiles
+			.InLine(index: indexer(position), indexer)
+			.OrderBy(selector);
 	}
 }
