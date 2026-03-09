56de595f3aa16fc75e223f1b2c9cfab06dad5556
eLilyPad <elizabethbryce00@gmail.com>
2026-03-09 13:21:06 +0000

    removed rotation

--- Nonogram/Display.Data.cs
+++ Nonogram/Display.Data.cs
@@ -141,7 +141,8 @@ public abstract partial class Display
 		}
 		public ImageTexture AsIcon(IColours colours, int pixelSize = 16) => Tiles
 			.AsIcon(getColor: colours.ChequeredNonogramTile, size: Size, pixelSize)
-			.Rotate90()
+			//.Rotate90()
+			//.FlipX()
 			.ToImage();
 
 		public IEnumerable<(Vector2I Position, Mode Mode)> InLine(HintPosition position)
