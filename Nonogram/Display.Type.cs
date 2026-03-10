67e83400141e8433692b1aa2f720cf1ea210479e
eLilyPad <elizabethbryce00@gmail.com>
2026-03-10 02:43:34 +0000

    removed unused

--- Nonogram/Display.Type.cs
+++ Nonogram/Display.Type.cs
@@ -20,27 +20,6 @@ public static class DisplayTypeExtensions
 		Type.Studio => save.Expected,
 		_ => save
 	};
-	public static Type ChangeType(this CurrentPuzzle puzzle, Type previous, Type current)
-	{
-		if (previous == current) return previous;
-		NonogramStudioBar studio = puzzle.UI.Studio;
-		Default display = puzzle.UI.Display;
-		display.Name = current.AsName();
-		switch (current)
-		{
-			case Type.Game:
-				display.Timer.Show();
-				studio.Hide();
-				if (previous is Type.Studio) puzzle.ClearPuzzle();
-				break;
-			case Type.Studio:
-				display.Timer.Hide();
-				studio.Show();
-				//puzzle.Puzzle = new() { Expected = new() };
-				break;
-		}
-		return current;
-	}
 	public static Type LogChange(this Type type)
 	{
 		Console.Console.Log($"Display changed too: {type.AsName()}");
