1b8dfcd54dcaee10d7a366038261539b0e628b57
eLilyPad <elizabethbryce00@gmail.com>
2026-03-19 00:08:44 +0000

    fun func

--- Nonogram/TileExtensions.cs
+++ Nonogram/TileExtensions.cs
@@ -2,6 +2,11 @@ namespace RSG.Nonogram;
 
 public static class TileExtensions
 {
+	public static T LockTiles<T>(this T values, bool locked) where T : IEnumerable<Tile>
+	{
+		foreach (Tile tile in values) tile.Locked = locked;
+		return values;
+	}
 	public static IEnumerable<KeyValuePair<T, Tile>> HoverTiles<T>(this IEnumerable<KeyValuePair<T, Tile>> tiles, bool value)
 	{
 		foreach ((T _, Tile tile) in tiles)
