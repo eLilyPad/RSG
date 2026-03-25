8cabd2c0104531a9b9554a7477ec57bd4dea2885
eLilyPad <elizabethbryce00@gmail.com>
2026-03-25 17:37:20 +0000

    made get or create virtual and added pool with no generic key

--- NodePool.cs
+++ NodePool.cs
@@ -2,10 +2,32 @@ using Godot;
 
 namespace RSG;
 
+public abstract class NodePool<TValue> : NodePool<string, TValue> where TValue : Node
+{
+	private const string defaultName = "";
+	public sealed override TValue GetOrCreate(string key)
+	{
+		TValue value = base.GetOrCreate(key);
+		value.Renamed += () => ChangeName(value);
+		value.TreeEntered += () => ChangeName(value);
+		return value;
+	}
+	private void ChangeName(TValue renamed)
+	{
+		var previous = _nodes.FirstOrDefault(
+			predicate: pair => pair.Value.GetInstanceId() == renamed.GetInstanceId(),
+			defaultValue: new(defaultName, default!)
+		);
+		if (previous is { Key: defaultName }) return;
+		if (previous.Key == renamed.Name) return;
+		_nodes[renamed.Name] = renamed;
+		_nodes.Remove(previous.Key);
+	}
+}
 public abstract class NodePool<TKey, TValue> where TKey : notnull where TValue : Node
 {
 	protected readonly Dictionary<TKey, TValue> _nodes = [];
-	public TValue GetOrCreate(TKey key) => _nodes.GetOrCreate(key, create: Create);
+	public virtual TValue GetOrCreate(TKey key) => _nodes.GetOrCreate(key, create: Create);
 	public void Clear(params IEnumerable<TKey> exceptions)
 	{
 		foreach ((TKey key, TValue node) in _nodes)
