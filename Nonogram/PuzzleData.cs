375187f933adb4815c8398f39044e231347c12e8
eLilyPad <elizabethbryce00@gmail.com>
2026-04-15 23:52:34 +0100

    removed unused

--- Nonogram/PuzzleData.cs
+++ Nonogram/PuzzleData.cs
@@ -139,19 +139,9 @@ public sealed class PuzzleData : Display.Data
 				Puzzles = [
 					new("Heart Emoji", selector: HeartEmoji, size) { DialogueName = Data.Intro},
 					new("Kitty", selector: Cat, size) { DialogueName = Data.CatOnThePath},
-					//new("Spiral", selector: Spiral, size),
-					new("Smiley Face", selector: SmileyEmoji, size),
-					//new("Noise", selector: position => position.IsOverNoiseThreshold(threshold: 0), size),
-					//new("Grid", selector: position => position.X % 3 == 0 || position.Y % 3 == 0, size),
-					//new("Border", selector: BorderSelector, size),
+					new("Smiley Face", selector: SmileyEmoji, size)
 				]
 			};
-
-			bool Spiral(Vector2I position)
-			{
-				const int lineThickness = 1, a = 4, b = 1;
-				return position.IsInSpiral(center: puzzleCenter, a, b, lineThickness);
-			}
 			bool HeartEmoji(Vector2I position)
 			{
 				const int curveRadius = (radius / 2) + 1, size = radius - 2;
